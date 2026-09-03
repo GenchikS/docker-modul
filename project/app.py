a4b4ba3f04312fd65a8bd8a4fec8e68523951ffc
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2026-09-03 23:15:28 +0300

    downloadPhotoStart

--- project/app.py
+++ project/app.py
@@ -9,12 +9,6 @@ file = open("./static/index.html", "r")
 html = file.read()
 file.close()
 
-# fileUpload = open("./static/upload.html", "r")
-# htmlUpload = fileUpload.read()
-# fileUpload.close()
-
-
-
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
@@ -24,7 +18,6 @@ class Handler(BaseHTTPRequestHandler):
             file_path = "./static" + self.path
 
         if os.path.isfile(file_path):
-
             with open(file_path, "rb") as file:
                 content = file.read()
 
@@ -43,19 +36,35 @@ class Handler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-
             self.wfile.write(b"404 - File not found")
-        
+  
 
-
-
-# class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
+
+        length = int(self.headers.get("Content-Length"))
+        body = self.rfile.read(length)
+        # flush=True - вивід відразу, щоб не було буферізації
+        # print(body, flush=True)
+
+        # код опрацювання завантаження файлу
+        boundary = self.headers["Content-Type"].split("boundary=")[-1].encode()
+        # відділення headers від body
+        start = body.find(b"\r\n\r\n") + 4
+        end = body.find(b"\r\n--" + boundary, start)
+
+        data = body[start:end]
+        file_download = open("./static/example.jpg", "wb")
+        file_download.write(data)
+        file_download.close()
+
         self.send_response(201)
         self.send_header("Content-Type", "text/plain")
         self.end_headers()
+        
         self.wfile.write(b"POST OK!")
-        # self.wfile.write(htmlUpload.encode())
+       
+
+
 
         
 
